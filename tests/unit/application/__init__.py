e3dcdaecbdf2205d0c4eb25d273dbd98b58b4e58
GaylordCC <gcarricaballero3@hotmail.com>
2026-09-17 01:10:38 -0500

    test: add application unit tests placeholder package

---
+++ tests/unit/application/__init__.py
@@ -0,0 +1,3 @@
+"""GUIDE: unit tests for application/use_cases/ go here, using a fake
+AgentPort implementation (no google.adk involved). Deferred until
+SendMessageUseCase.execute() has real orchestration logic to verify."""
