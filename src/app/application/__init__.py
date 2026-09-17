82023a421fb641cb2d65d83fffb729e4e38d446a
GaylordCC <gcarricaballero3@hotmail.com>
2026-09-17 01:10:19 -0500

    feat: add application layer package

---
+++ src/app/application/__init__.py
@@ -0,0 +1,3 @@
+"""Application layer: orchestrates domain entities and ports to fulfill
+a use case. Knows the SEQUENCE of steps; never knows HOW a step is
+actually implemented (that's infrastructure's job)."""
